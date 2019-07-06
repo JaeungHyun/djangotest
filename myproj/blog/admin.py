eb8d79dbbf30b640dd6c6b6c4741f1424da735da
JaeUng Hyun <jaeung@me.com>
2019-07-06 17:23:28 +0900

    blog.Post 모델을 admin에 등록

--- myproj/blog/admin.py
+++ myproj/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content']
+    list_filter = ["update_at"]
+    search_fields = ['title']
